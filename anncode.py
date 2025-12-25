98b63dbd68634cd59950659a2e71afe3c8b08bc9
unknown <0olany15@solent.ac.uk>
2025-12-25 16:41:52 +0000

    create training data

--- anncode.py
+++ anncode.py
@@ -55,3 +55,38 @@ classes = sorted(set(classes))
 # Save workds and classes to pickle files
 pickle.dump(words, open("words.pkl", "wb"))
 pickle.dump(classes, open("classes.pkl", "wb"))
+
+
+# Create training data
+
+training = []
+
+output_empty = [0] * len(classes)
+
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+
+    # lemmatize each word in the pattern
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+
+    # Create bag of words, 0 if word does not exists and 1 if it exists
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+
+    # Create output row, i.e tag one-hot encoding
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+
+    training.append([bag, output_row])
+
+
+# randomized the training data
+random.shuffle(training)
+
+# Convert training to numpy array
+training = np.array(training, dtype=object)
+
+# split training data to train and test i.e X,y
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
